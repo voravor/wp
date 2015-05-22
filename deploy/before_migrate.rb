c210154d284cc0abfee0066e13c057ba438fcfda
voravor <voravor@gmail.com>
2015-05-22 12:30:15 -0400

    turn off composer for now

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -3,13 +3,13 @@
         level :debug
     end
     # run composer install
-    execute "/usr/bin/php /usr/local/bin/composer install" do
-        user "deploy"
-        cwd path
-        command "composer install --no-dev --no-interaction --optimize-autoloader"
-        # only execute for composer projects
-        only_if "test -f \"#{path}/composer.json\""
-    end
+  #  execute "/usr/bin/php /usr/local/bin/composer install" do
+  #      user "deploy"
+  #      cwd path
+  #      command "composer install --no-dev --no-interaction --optimize-autoloader"
+  #      # only execute for composer projects
+  #      only_if "test -f \"#{path}/composer.json\""
+  #  end
     
     
     
